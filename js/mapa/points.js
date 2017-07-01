7fec69ec4abeb6b444d3ded1e88251968ca3fa79
Claudio Sepúlveda Geoffroy <claudio.sepulveda.geoffroy@gmail.com>
2017-07-01 18:30:26 -0400

    Update

--- js/mapa/points.js
+++ js/mapa/points.js
@@ -1,14 +1,54 @@
-var RipleyMetropolitana = [
+var Neltume_Cliente_1 = [
     {
-        lat: -33.50177,
-        lon: -70.75719,
+        lat: -33.706047,
+        lon: -71.124488,
         zoom: 15,
-        title: 'Sensor 24Z7',
+        title: 'Sensor 11A1',
         icon: 'images/mapas.png',
         html: [
-            '<h3>Sensor 24Z7</h3>',
-            '<p><b>Ramón Freire (Avda. Los Pajaritos) 2753, Maipú</b><br>Horario Atención: 24 Horas.</p>'
+            '<h3>11A1</h3>',
+            '<p><b>Zona A Nodo 1</b><br>Última actualización: 01 Julio 2017 22:30 hrs.</p><br><a href="#" class="btn btn-primary">Ver detalle</a>'
         ].join(''),
-        animation: google.maps.Animation.DROP
+        animation: google.maps.Animation.DROP,
+        // mapTypeId: google.maps.MapTypeId.SATELLITE
+    },
+    {
+        lat: -33.699631,
+        lon: -71.124324,
+        zoom: 15,
+        title: 'Sensor 11A2',
+        icon: 'images/mapas.png',
+        html: [
+            '<h3>11A2</h3>',
+            '<p><b>Zona A Nodo 2</b><br>Última actualización: 01 Julio 2017 22:30 hrs.</p><br><a href="#" class="btn btn-primary">Ver detalle</a>'
+        ].join(''),
+        animation: google.maps.Animation.DROP,
+        // mapTypeId: google.maps.MapTypeId.SATELLITE
+    },
+    {
+        lat: -33.706012,
+        lon: -71.119711,
+        zoom: 15,
+        title: 'Sensor 11A3',
+        icon: 'images/mapas.png',
+        html: [
+            '<h3>11A3</h3>',
+            '<p><b>Zona A Nodo 3</b><br>Última actualización: 01 Julio 2017 22:30 hrs.</p><br><a href="#" class="btn btn-primary">Ver detalle</a>'
+        ].join(''),
+        animation: google.maps.Animation.DROP,
+        // mapTypeId: google.maps.MapTypeId.SATELLITE
+    },
+    {
+        lat: -33.701060,
+        lon: -71.118921,
+        zoom: 15,
+        title: 'Sensor 11A4',
+        icon: 'images/mapas.png',
+        html: [
+            '<h3>11A4</h3>',
+            '<p><b>Zona A Nodo 4</b><br>Última actualización: 01 Julio 2017 22:30 hrs.</p><br><a href="#" class="btn btn-primary">Ver detalle</a>'
+        ].join(''),
+        animation: google.maps.Animation.DROP,
+        // mapTypeId: google.maps.MapTypeId.SATELLITE
     },
 ];
