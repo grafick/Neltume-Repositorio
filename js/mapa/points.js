0530693a8dbf05166faaf3d289e97bed8022b4da
Claudio Sepúlveda Geoffroy <claudio.sepulveda.geoffroy@gmail.com>
2017-07-01 16:43:44 -0400

    Mapa

---
+++ js/mapa/points.js
@@ -0,0 +1,14 @@
+var RipleyMetropolitana = [
+    {
+        lat: -33.50177,
+        lon: -70.75719,
+        zoom: 15,
+        title: 'Sensor 24Z7',
+        icon: 'images/mapas.png',
+        html: [
+            '<h3>Sensor 24Z7</h3>',
+            '<p><b>Ramón Freire (Avda. Los Pajaritos) 2753, Maipú</b><br>Horario Atención: 24 Horas.</p>'
+        ].join(''),
+        animation: google.maps.Animation.DROP
+    },
+];
